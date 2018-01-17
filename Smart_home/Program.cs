e5e75a84591b71b392383e5a62a20b6e96515a32
Robert Murvai <robert.murvai@bullguard.com>
2018-01-17 12:29:20 +0200

    add project files

---
+++ Smart_home/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_http
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string myFolder = @"E:\Licenta_februarie2018\Source\web_root";
+            SimpleHTTPServer myServer;
+
+            //create server with auto assigned port
+            myServer = new SimpleHTTPServer(myFolder);
+
+
+            //Creating server with specified port
+            //myServer = new SimpleHTTPServer(myFolder, 8085);
+
+
+            //Now it is running:
+            Console.WriteLine("Server is running on this port: " + myServer.Port.ToString());
+
+            for (; ; );
+
+
+            //Stop method should be called before exit.
+            myServer.Stop();
+
+        }
+    }
+}
