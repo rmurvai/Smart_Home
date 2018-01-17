264682eeb300f19a07641813afdb9608f4e71d17
robert <rmurvai@gmail.com>
2018-01-18 01:41:59 +0200

    add some comments

--- Smart Home Sensors/Licenta/Licenta.ino
+++ Smart Home Sensors/Licenta/Licenta.ino
@@ -8,118 +8,159 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>;
 
-//Constants
-#define DHTPIN 7     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-DHT dht( DHTPIN, DHTTYPE ); //// Initialize DHT sensor for normal 16mhz Arduino
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+// Constants
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+#define DHTPIN  7       // set the pin for the temperature sensor
+#define DHTTYPE DHT22   // set the type of the temperature sensor => DHT 22  (AM2302)
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+// Initialize DHT sensor for normal 16mhz Arduino
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+DHT dht( DHTPIN, DHTTYPE );  // init
 
 
-//Variables
-int chk;
-float hum;  //Stores humidity value
-float temp; //Stores temperature value
-
-/////////////////////////////
-//VARS
-//the time we give the sensor to calibrate (10-60 secs according to the datasheet)
-int calibrationTime = 30;
-
-//the time when the sensor outputs a low impulse
-long unsigned int lowIn;
-
-//the amount of milliseconds the sensor has to be low 
-//before we assume all motion has stopped
-long unsigned int pause = 5000;
-
-boolean lockLow = true;
-boolean takeLowTime;
-
-int pirPin = 3;    //the digital pin connected to the PIR sensor's output
-int ledPin = 13;
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+// Global Variables
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+float   dht_humidity_value;        //Stores humidity value
+float   dht_temperature_value;     //Stores temperature value
+int     dht_calibration_time = 30; //the time we give the sensor to calibrate (10-60 secs according to the datasheet)
 
 
-/////////////////////////////
+long unsigned int pir_low_In;           //the time when the sensor outputs a low impulse
+long unsigned int pir_pause_time = 5000;//the amount of milliseconds the sensor has to be low before we assume all motion has stopped
+int               pir_Pin = 3;          //the digital pin connected to the PIR sensor's output
+boolean           pir_lock_Low = true;  //control variable for low impulse
+boolean           pir_take_Low_time;    //control variable for low time
+int               pir_control_led_Pin = 13;// the pin where our pir led is connected
+
+
+//
+// Calibration pir sensor
+//
+void CalibratePirSensor()
+{
+    //give the sensor some time to calibrate
+    Serial.print("Start calibrating movement sensor ");
+    for (int i = 0; i < dht_calibration_time; i++)
+    {
+        Serial.print(".");
+        delay(1000);
+    }
+    Serial.println(" End calibrating movement sensor");
+    Serial.println("SENSOR ACTIVE");
+    delay(50);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
 //SETUP
+//
+/////////////////////////////////////////////////////////////////////////////////////////
 void setup()
 {
     Serial.begin( 9600 );
-    //////////////////////////////////////////////
-    // humidity & temp sensor
+    
+    //
+    // start monitoring humidity and temperature sensor
+    //
     dht.begin();
 
-    //////////////////////////////////////////////
+    //
     // pir movement sensor setup
-    pinMode( pirPin, INPUT );
-    pinMode( ledPin, OUTPUT );
-    digitalWrite( pirPin, LOW );
+    //
+    pinMode( pir_control_led_Pin, OUTPUT );
+    pinMode( pir_Pin, INPUT );
+    digitalWrite( pir_Pin, LOW );
 
-    //give the sensor some time to calibrate
-    Serial.print( "calibrating sensor " );
-    for ( int i = 0; i < calibrationTime; i++ )
-    {
-        Serial.print( "." );
-        delay( 1000 );
-    }
-    Serial.println( " done" );
-    Serial.println( "SENSOR ACTIVE" );
-    delay( 50 );
+    //
+    // calibrate the pir sensor
+    //
+    CalibratePirSensor();
+
+
 }
 
-////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+//
 //LOOP
+//
+/////////////////////////////////////////////////////////////////////////////////////////
 void loop()
 {
-
-    if ( digitalRead( pirPin ) == HIGH )
+    //
+    // check data for pir sensor
+    //
+    if ( digitalRead( pir_Pin ) == HIGH )
     {
-        digitalWrite( ledPin, HIGH );   //the led visualizes the sensors output pin state
-        if ( lockLow )
+        digitalWrite( pir_control_led_Pin, HIGH );   // the led visualizes the sensors output pin state
+        if ( pir_lock_Low )
         {
-            //makes sure we wait for a transition to LOW before any further output is made:
-            lockLow = false;
+            //
+            // makes sure we wait for a transition to LOW before any further output is made:
+            //
+            pir_lock_Low = false;
             Serial.println( "---" );
             Serial.print( "motion detected at " );
             Serial.print( millis() / 1000 );
             Serial.println( " sec" );
             delay( 50 );
         }
-        takeLowTime = true;
+        pir_take_Low_time = true;
     }
 
-    if ( digitalRead( pirPin ) == LOW )
+    if ( digitalRead( pir_Pin ) == LOW )
     {
-        digitalWrite( ledPin, LOW );  //the led visualizes the sensors output pin state
+        digitalWrite( pir_control_led_Pin, LOW );  // the led visualizes the sensors output pin state
 
-        if ( takeLowTime )
+        if ( pir_take_Low_time )
         {
-            lowIn = millis();          //save the time of the transition from high to LOW
-            takeLowTime = false;       //make sure this is only done at the start of a LOW phase
+            pir_low_In        = millis();    // save the time of the transition from high to LOW
+            pir_take_Low_time = false;       // make sure this is only done at the start of a LOW phase
         }
-        //if the sensor is low for more than the given pause, 
-        //we assume that no more motion is going to happen
-        if ( !lockLow && millis() - lowIn > pause )
+
+        //
+        // if the sensor is low for more than the given pause, we assume that no more motion is going to happen
+        //
+        if ( !pir_lock_Low && millis() - pir_low_In > pir_pause_time )
         {
-            //makes sure this block of code is only executed again after 
-            //a new motion sequence has been detected
-            lockLow = true;
+            //
+            //makes sure this block of code is only executed again after a new motion sequence has been detected
+            //
+            pir_lock_Low = true;
             Serial.print( "motion ended at " );      //output
-            Serial.print( ( millis() - pause ) / 1000 );
+            Serial.print( ( millis() - pir_pause_time ) / 1000 );
             Serial.println( " sec" );
             delay( 50 );
         }
     }
 
-    // delay( 2000 );
-    //Read data and store it to variables hum and temp
-    hum = dht.readHumidity();
-    temp = dht.readTemperature();
-    //Print temp and humidity values to serial monitor
+    //
+    // get data from the dht sensor
+    //
+    delay( 2000 ); // wait 2 sec to get data from sensor 
+
+    //
+    // read data and store it to variables dht_humidity_value and dht_temperature_value
+    //
+    dht_humidity_value    = dht.readHumidity();
+    dht_temperature_value = dht.readTemperature();
+
+    //
+    // Print temp and humidity values to serial monitor
+    //
     Serial.print( "Humidity: " );
-    Serial.print( hum );
+    Serial.print( dht_humidity_value );
     Serial.print( " %, Temp: " );
-    Serial.print( temp );
+    Serial.print( dht_temperature_value );
     Serial.println( " Celsius" );
-    //delay( 10000 ); //Delay 2 sec.
 
 }
 
